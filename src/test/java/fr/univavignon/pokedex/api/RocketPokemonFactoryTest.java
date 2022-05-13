9eb74f5d80b438ad7a4da45f3d95d0ac89113a8c
Jacob Michalski <jakub.michalski@alumni.univ-avignon.fr>
2022-05-13 09:23:37 +0200

    RocketPokemonFactory test

--- src/test/java/fr/univavignon/pokedex/api/RocketPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/RocketPokemonFactoryTest.java
@@ -8,10 +8,11 @@ class RocketPokemonFactoryTest {
 
     @Test
     void createPokemon() {
-        IPokemonFactory pokemonFactory = new PokemonFactory();
+        IPokemonFactory pokemonFactory = new RocketPokemonFactory();
         assert(pokemonFactory.createPokemon(0, 613, 64, 4000, 4).getIndex() == 0);
         assert(pokemonFactory.createPokemon(133, 2729, 202, 5000, 4).getIndex() == 133);
         assert(pokemonFactory.createPokemon(-1, 613, 64, 4000, 4) == null);
         assert(pokemonFactory.createPokemon(151, 613, 64, 4000, 4) == null);
+
     }
 }
